6d2db2996bad2b0f2f9f4761f92b7bd8b187864b
alok27a <alok27a@gmail.com>
2022-08-26 09:28:34 +0530

    Pie chart added in dashboard

--- src/Components/Dashboard/UserRoutes/ShowPapers.js
+++ src/Components/Dashboard/UserRoutes/ShowPapers.js
@@ -2,8 +2,57 @@ import React from "react";
 import { Button, Flex, Text } from "@chakra-ui/react";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import { useNavigate } from "react-router-dom";
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, Tooltip, ArcElement, Title, Legend } from 'chart.js';
+
+ChartJS.register({
+  Tooltip, Title, ArcElement, Legend
+})
+
 function ShowPapers() {
   const navigate = useNavigate();
+  const data = {
+    color: 'white',
+    labels: [
+      'Funded',//Green
+      'Rejected', //Red
+      'Submitted',//orange
+      'Drafts' // white
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [3, 5, 1,4],
+      backgroundColor: [
+        'rgb(50,205,50)',
+        'rgb(255,0,0)',
+        'rgb(255, 197, 92)',
+        'rgb(0, 162, 237)',
+      ],
+      hoverOffset: 4
+    }]
+  };
+  const options = {
+    plugins: {
+      title: {
+        display: true,
+        text: 'Statistics',
+        color: 'white',
+        font: {
+          size: 34
+        },
+        padding: {
+          top: 30,
+          bottom: 30
+        },
+        responsive: true,
+        animation: {
+          animateScale: true,
+        }
+      }
+    }
+
+  }
+
   return (
     <Flex align="center">
       <Flex
@@ -23,14 +72,14 @@ function ShowPapers() {
           Go to Drafts
         </Button>
         <Button leftIcon={<VisibilityIcon />} onClick={() => {
-            navigate("/dashboard/submitted");
-          }} w="30vw" h="10vh">View Submitted Proposals</Button>
+          navigate("/dashboard/submitted");
+        }} w="30vw" h="10vh">View Submitted Proposals</Button>
         <Button leftIcon={<VisibilityIcon />} onClick={() => {
-            navigate("/dashboard/funded");
-          }} w="30vw" h="10vh">View Funded Proposals</Button>
+          navigate("/dashboard/funded");
+        }} w="30vw" h="10vh">View Funded Proposals</Button>
       </Flex>
-      <Flex w="50%" align="center" justify="center">
-        <Text>PIE CHART</Text>
+      <Flex w="35%" align="center" justify="center">
+        <Doughnut data={data} options={options} height={200} />
       </Flex>
     </Flex>
   );
